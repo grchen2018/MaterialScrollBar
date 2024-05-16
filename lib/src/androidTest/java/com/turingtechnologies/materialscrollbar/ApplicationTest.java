6ba4a36ceb6c3d338f54fd546baac32a33c1fd2f
CGR <grchen2018@github.com>
2024-05-16 19:05:40 +0800

    Fix build issue

--- lib/src/androidTest/java/com/turingtechnologies/materialscrollbar/ApplicationTest.java
+++ lib/src/androidTest/java/com/turingtechnologies/materialscrollbar/ApplicationTest.java
@@ -16,9 +16,7 @@
 
 package com.turingtechnologies.materialscrollbar;
 
-import android.test.ActivityTestCase;
-
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ActivityTestCase {}
+public class ApplicationTest {}
